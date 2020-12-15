72d3e2437606b6aab18f1179895ed0560025ed8f
liuliangdong <liuliangdong@qiyandata.com>
2020-12-15 17:50:41 +0800

    20201214

---
+++ others/1.py
@@ -0,0 +1,13 @@
+import time
+from numba import jit
+
+
+@jit
+def foo(x,y):
+    tt = time.time()
+    s = 0
+    for i in range(x,y):
+        s += i
+    print('Time used: {} sec'.format(time.time()-tt))
+    return s
+print(foo(1,100000000))
